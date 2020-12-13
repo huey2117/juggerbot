c4b4cee9df3f72c007c5dc6383f62ca774aebd3f
Michael Huey <michaelyeuh@gmail.com>
2020-12-13 12:30:06 -0500

    added logging and cleaned up role assignment

--- bot.py
+++ bot.py
@@ -7,21 +7,24 @@ import logging
 import discord
 from discord.ext import commands
 
-log = logging.getLogger('jbot')
-log.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='jbot.log', encoding='utf-8', mode='w')
-handler.setFormatter(
-    logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
+logging.basicConfig(
+    filename='jbot.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - [%(levelname)s]: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
-
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
-guild = os.getenv('DISCORD_GUILD')
+target_guild = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.presences = True
+intents.members = True
 
 # either client, client.run(token)
-client = discord.Client()
+# client = discord.Client()
 # or bot, bot.run(token)
-bot = commands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix='!', intents=intents)
 
 
 @client.event
@@ -29,10 +32,7 @@ async def on_connect():
     """
     log connected status
     """
-    for g in client.guilds:
-        if g.name == guild:
-            break
-    log.info(f'{client.user} has connected to {g.name}')
+    logging.info(f'{client.user} is connected')
 
 
 @client.event
@@ -40,30 +40,36 @@ async def on_ready():
     """
     log ready status
     """
-    for g in client.guilds:
-        if g.name == guild:
+    for guild in client.guilds:
+        if guild.name == target_guild:
             break
 
-    log.info(f'{client.user} is ready on {g.name}')
+    logging.info(f'{client.user} is ready on {guild.name}')
 
 
 @client.event
 async def on_member_join(member):
     """
-    On new user join, set them to X role which only allows them to view the
+    On new user join, set them to a role which only allows them to view the
     #welcome channel. They will be required to respond to the Welcome Message
     with a reaction in order to access the rest of the discord.
     """
-    await member.create_dm()
-    await member.dm_channel.send('welcome message')
+    role = discord.utils.get(member.guild.roles, name='Astral Sea')
+    logging.info(f'Add {member.display_name} to {role}')
+    await member.add_roles(role)
+
+    # Saving the below because I think I want to implement this functionality
+    # elsewhere, but I do not yet have that spot created so I want to use
+    # this as a reminder
+    # await member.create_dm()
+    # await member.dm_channel.send('welcome message')
 
 
 @client.event
 async def on_error(event, *args, **kwargs):
     trace = sys.exc_info()[2]
-    dt_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     if event == 'on_message':
-        log.error(f'Unhandled message: {args[0]}\n{trace}')
+        logging.error(f'Unhandled message: {args[0]}\n{trace}')
     else:
         raise
 
