b365ae638605466c12b6c734da9b8cb6f287b998
Michael Huey <michaelyeuh@gmail.com>
2020-12-20 12:10:09 -0500

    added role removal as well

--- bot.py
+++ bot.py
@@ -26,6 +26,7 @@ intents.reactions = True
 # Initialize bot
 client = commands.Bot(command_prefix='!', intents=intents)
 
+
 @client.event
 async def on_connect():
     """
@@ -79,33 +80,77 @@ async def on_raw_reaction_add(payload):
     user = await client.fetch_user(payload.user_id)
     channel = await client.fetch_channel(payload.channel_id)
 
-    try:
-        member = payload.member
-        # This section handles the welcome role add reactions
-        welcome_id = discord.utils.get(member.guild.channels, name="welcome")
-        if channel == welcome_id:
-            role = None
-            logging.info(f"{user.name} reacted to welcome message")
-            print(f"{user.name} reacted with {reaction}: {reaction.id}")
+    member = payload.member
+    # This section handles the welcome role add reactions
+    welcome_id = discord.utils.get(member.guild.channels, name="welcome")
+    if channel == welcome_id:
+        logging.info(f"{user.name} reacted to welcome message")
 
-            # Create key value pairs of reaction id and role name pairs
-            reaction_to_role = {}
-            role_reactions = ['wow', 'd20', 'mtg', 'sus', 'press_F']
-            role_names = ['WoW', 'Tabletop', 'MtG', 'sus AF', 'Commoner']
-            for react, rname in zip(role_reactions, role_names):
-                for e in client.emojis:
-                    if e.name == react:
-                        reaction_id = client.get_emoji(e.id)
-                        reaction_to_role[reaction_id.id] = discord.utils.get(
-                            member.guild.roles,
-                            name=rname
-                        )
-            if reaction.id in reaction_to_role:
-                role = reaction_to_role[reaction.id]
+        # Create key value pairs of reaction id and role name pairs
+        reaction_to_role = {}
+        role_reactions = ['wow', 'd20', 'mtg', 'sus', 'press_F']
+        role_names = ['WoW', 'Tabletop', 'MtG', 'sus AF', 'Commoner']
+        for react, rname in zip(role_reactions, role_names):
+            for e in client.emojis:
+                if e.name == react:
+                    reaction_id = client.get_emoji(e.id)
+                    reaction_to_role[reaction_id.id] = discord.utils.get(
+                        member.guild.roles,
+                        name=rname
+                    )
+        if reaction.id in reaction_to_role:
+            role = reaction_to_role[reaction.id]
+            if role in member.roles:
+                logging.info(f"Not adding {user.name} to {role} "
+                             f"as they are already a member."
+                             )
+            else:
                 logging.info(f"Adding {user.name} to {role}")
                 await member.add_roles(role)
-    except ValueError:
-        logging.warning("No member payload available")
+
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    reaction = payload.emoji
+    user = await client.fetch_user(payload.user_id)
+    channel = await client.fetch_channel(payload.channel_id)
+
+    # The remove action often comes without a payload, so I will attempt
+    # to grab the member object from the user id.
+    # get_all_members() returns a list, so I need the 0th element since
+    # only one member should match a specific action
+    member = payload.member or (
+        [m
+         for m in client.get_all_members()
+         if m.id == user.id
+         ][0]
+    )
+    # This section handles the welcome role removal reactions
+    welcome_id = discord.utils.get(member.guild.channels, name="welcome")
+    if channel == welcome_id:
+        logging.info(f"{user.name} removed a reaction to welcome message")
+
+        # Create key value pairs of reaction id and role name pairs
+        reaction_to_role = {}
+        role_reactions = ['wow', 'd20', 'mtg', 'sus', 'press_F']
+        role_names = ['WoW', 'Tabletop', 'MtG', 'sus AF', 'Commoner']
+        for react, rname in zip(role_reactions, role_names):
+            for e in client.emojis:
+                if e.name == react:
+                    reaction_id = client.get_emoji(e.id)
+                    reaction_to_role[reaction_id.id] = discord.utils.get(
+                        member.guild.roles,
+                        name=rname
+                    )
+        if reaction.id in reaction_to_role:
+            role = reaction_to_role[reaction.id]
+            if role in member.roles:
+                logging.info(f"Removing {user.name} from {role}")
+                await member.remove_roles(role)
+            else:
+                logging.info(f"Not removing {user.name} from {role} "
+                             f"as they are not a member."
+                             )
 
 
 def main():
